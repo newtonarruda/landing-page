28835a488ea099d7b5a91c2cd7e3eb2b8ddaf2b1
Newton Arruda <newton.arruda@gmail.com>
2018-09-07 17:47:22 -0300

    Esqueleto

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    $("#includedContent").load("b.html");
+});
+
+$(function() {
+    var includes = $('[data-include]');
+    jQuery.each(includes, function() {
+        var file = 'views/' + $(this).data('include') + '.html';
+        $(this).load(file);
+    });
+});
